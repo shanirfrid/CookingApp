3ec17774307f7b52755c53c12779968d06066cba
idanv101 <brade1997@gmail.com>
2022-01-16 17:16:27 +0200

    feat[Recipe Details]; Update recipe details page

---
+++ app/src/main/java/com/example/shanir/cookingappofshanir/classs/TextFormatter.java
@@ -0,0 +1,11 @@
+package com.example.shanir.cookingappofshanir.classs;
+
+public class TextFormatter {
+
+    public static String formatRecipeTime(int minutes) {
+        if (minutes < 60)
+            return String.format("%dm", minutes);
+
+        return String.format("%dh %02dm", minutes / 60, minutes % 60);
+    }
+}
