8af57bc290050bfc3aa1ba380d25113471ed1eec
idanv101 <brade1997@gmail.com>
2022-01-26 19:54:28 +0200

    Enhancement[Time]: Remove leading zero from the minutes in recipe time

--- app/src/main/java/com/example/shanir/cookingappofshanir/utils/TextFormatter.java
+++ app/src/main/java/com/example/shanir/cookingappofshanir/utils/TextFormatter.java
@@ -6,7 +6,7 @@ public class TextFormatter {
         if (minutes < 60)
             return String.format("%dm", minutes);
 
-        return String.format("%dh %02dm", minutes / 60, minutes % 60);
+        return String.format("%dh %dm", minutes / 60, minutes % 60);
     }
 
     public static String foundRecipesNumber(int recipesAmount)
